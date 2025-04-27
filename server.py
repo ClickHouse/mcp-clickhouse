1abc991bb33bad3765d7bae60f6be708d2625edb
Jasim Muhammed <hello@jasimmk.com>
2025-04-27 22:30:48 +0400

    fix: port

--- server.py
+++ server.py
@@ -2,4 +2,4 @@ import uvicorn
 from mcp_clickhouse.mcp_server import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=18213)
