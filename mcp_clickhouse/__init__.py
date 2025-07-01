db49c69bb6ebb1f402fc435edccd917f344394b6
remonxiao <remonxiao@tencent.com>
2025-07-01 12:25:15 +0800

    Fix

--- mcp_clickhouse/__init__.py
+++ mcp_clickhouse/__init__.py
@@ -5,10 +5,8 @@ from .mcp_server import (
     run_select_query,
     create_chdb_client,
     run_chdb_select_query,
-    list_clickhouse_servers,
-    create_chdb_client,
-    run_chdb_select_query,
     chdb_initial_prompt,
+    list_clickhouse_servers,
 )
 from .mcp_env import get_config, get_all_configs, get_mcp_server_config
 
@@ -19,11 +17,9 @@ __all__ = [
     "create_clickhouse_client",
     "create_chdb_client",
     "run_chdb_select_query",
+    "chdb_initial_prompt",
     "list_clickhouse_servers",
     "get_config",
     "get_all_configs",
     "get_mcp_server_config",
-    "create_chdb_client",
-    "run_chdb_select_query",
-    "chdb_initial_prompt",
 ]
